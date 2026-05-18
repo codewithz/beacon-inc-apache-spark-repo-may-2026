c6df19b8534ee2b1abefd67bae308b3c7bcb70d6
Zartab M Nakhwa <zartab@codewithz.com>
2026-05-18 08:50:01 +0530

    Create RDD

---
+++ src/main/java/com/beacon/CreateRDD.java
@@ -0,0 +1,39 @@
+package com.beacon;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CreateRDD {
+
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("RDD Creation App")
+                .master("local[4]")
+                .getOrCreate();
+
+
+        JavaSparkContext context= JavaSparkContext.fromSparkContext(spark.sparkContext());
+
+
+        List<Integer> list= Arrays.asList(1,2,3,4,5);
+
+        JavaRDD<Integer> rdd=context.parallelize(list);
+
+        int numberOfPartitions= rdd.getNumPartitions();
+        System.out.println("Number of partitions: " + numberOfPartitions);
+
+        List<Integer> output= rdd.collect();
+        System.out.println("RDD contents: " + output);
+
+
+
+        try (final var scanner = new Scanner(System.in)) {
+            scanner.nextLine();
+        }
+    }
+}
